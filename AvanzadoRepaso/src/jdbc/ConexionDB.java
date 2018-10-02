8014304f25be8482fdfa0a6549d56c797e1084c8
Martin <mlennard@gmail.com>
2018-10-01 22:12:26 -0300

    Connection test, statement and preparedstatement (parametros de la consulta).

--- AvanzadoRepaso/src/jdbc/ConexionDB.java
+++ AvanzadoRepaso/src/jdbc/ConexionDB.java
@@ -2,7 +2,10 @@ package jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+
 
 public class ConexionDB {
 
@@ -18,22 +21,65 @@ public class ConexionDB {
 		}
 
 		System.out.println("MySQL JDBC Driver Registered!");
-		Connection connection = null;
+		
+		try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/java", "java2", "utn");) {
+			
+			//Statement stmt = connection.createStatement();
+			//String query_no_param = "SELECT * FROM alumnos where edad";
+			String query = "SELECT * FROM alumnos where edad > ?";
+			PreparedStatement pstmt = connection.prepareStatement(query);
+			
+			pstmt.setInt(1, 30);
+			
+			ResultSet rs = pstmt.executeQuery();
+			//ResultSet rs = stmt.executeQuery(query_no_param);
+			
+			int id, edad;
+			String nombre;
+			
+			int numCols = rs.getMetaData().getColumnCount();
+			String[] tipoDatos = new String[numCols];
+			String[] nombreColumnas = new String[numCols];
+			
+			for (int i = 0; i < numCols; i++) {
+				tipoDatos[i] = rs.getMetaData().getColumnTypeName(i+1);
+				nombreColumnas[i] = rs.getMetaData().getColumnName(i+1);
+			}
+			
+			for (int i = 0; i < numCols; i++) {
+				System.out.println(nombreColumnas[i] + " es " + tipoDatos[i] );
+			}
+			
+			while(rs.next()) {
+				id = rs.getInt("ID_ALUMNO");
+	            nombre = rs.getString("NOMBRE");
+	            
+	            edad = rs.getInt("edad");
+	            System.out.println(String.format("id: %d, nombre: %s, edad: %d" , id, nombre, edad));  
 
-		try {
-			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/java", "java", "java");
+			}
+		
+			if (connection != null) {
+				System.out.println("You made it, take control your database now!");
+			} 	
+			
+		} catch (SQLException ex) {
+			while(ex != null) {
+		         System.out.println("SQLState:  " + ex.getSQLState());
+		         System.out.println("Error Code:" + ex.getErrorCode());
+		         System.out.println("Message:   " + ex.getMessage());
+		         Throwable t = ex.getCause();
+		         while(t != null) {
+		             System.out.println("Cause:" + t);
+		             t = t.getCause();
+		         }
+		         ex = ex.getNextException();
+		    }
 
-		} catch (SQLException e) {
-			System.out.println("Connection Failed! Check output console");
-			e.printStackTrace();
 			return;
 		}
 
-		if (connection != null) {
-			System.out.println("You made it, take control your database now!");
-		} else {
-			System.out.println("Failed to make connection!");
-		}
+		
 	}
 
 }
